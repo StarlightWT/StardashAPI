ce9a382cbcea8b46a75706ab84f59330c84b7357
Starlight <StarlightWT@protonmail.com>
2024-02-27 10:15:57 +0100

    Added test connect?

--- src/databaseHandler.js
+++ src/databaseHandler.js
@@ -1,24 +1,25 @@
 const mariadb = require("mariadb");
 
+console.log(process.env.DB_SERVER_IP);
 const pool = mariadb.createPool({
 	host: process.env.DB_SERVER_IP,
 	user: process.env.DB_ROLE,
 	password: process.env.DB_PASSWORD,
-	connectionLimit: 5,
+	database: "test",
 });
 
 async function testConnection() {
 	let conn;
 	try {
-		conn = await pool.getConnection();
-		const test = await conn.query("SELECT *");
-		console.log(test);
+		conn = await mariadb.createConnection(pool);
 		return 1;
 	} catch (e) {
 		console.error(e);
 		return 0;
 	} finally {
-		if (conn) return conn.end();
+		if (conn) {
+			await conn.end();
+		}
 	}
 }
 
