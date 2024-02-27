f2dd2e041dd4f29dd22b97f287be73b0018cfa0b
Starlight <StarlightWT@protonmail.com>
2024-02-27 12:32:57 +0100

    Made DB connection fully functional

--- src/databaseHandler.js
+++ src/databaseHandler.js
@@ -1,17 +1,17 @@
 const mariadb = require("mariadb");
 
-console.log(process.env.DB_SERVER_IP);
 const pool = mariadb.createPool({
-	host: process.env.DB_SERVER_IP,
+	host: process.env.DB_HOST,
 	user: process.env.DB_ROLE,
 	password: process.env.DB_PASSWORD,
 	database: "test",
 });
 
 async function testConnection() {
+	console.log(`I:[PR] DB IP=${process.env.DB_HOST}`);
 	let conn;
 	try {
-		conn = await mariadb.createConnection(pool);
+		conn = await pool.getConnection();
 		return 1;
 	} catch (e) {
 		console.error(e);
