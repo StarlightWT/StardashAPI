4061633fe8c6ddbb54c41e8d0cda77006986d35b
Starlight <StarlightWT@protonmail.com>
2024-02-27 13:02:12 +0100

    modified databaseHandler to accept creation of users

--- handlers/user/getInfo.js
+++ handlers/user/getInfo.js
@@ -3,7 +3,6 @@ const db = require("../../src/databaseHandler");
 module.exports = (req, res) => {
 	const { id } = req.params;
 
-	// TODO get data from DB and return the data
 	db.getUser(id).then((user) => {
 		if (user) res.status(200).send(user);
 		else res.sendStatus(404).send({ message: "User not found!" });
